c3cf531221fdd674d410abbfac5d7d63cd95d8e0
eseneosagie <eseneosagie@punch.cool>
2022-10-07 17:10:59 +0100

    commit

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,12 @@ import uploadRoute from './routes/uploadRoute';
 
 const mongodbUrl = config.MONGODB_URL;
 mongoose
-  .connect(mongodbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .catch((error) => console.log(error.reason));
+    .connect(mongodbUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .catch((error) => console.log(error.reason));
 
 const app = express();
 app.use(bodyParser.json());
@@ -24,14 +24,14 @@ app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/orders', orderRoute);
 app.get('/api/config/paypal', (req, res) => {
-  res.send(config.PAYPAL_CLIENT_ID);
+    res.send(config.PAYPAL_CLIENT_ID);
 });
 app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
 app.use(express.static(path.join(__dirname, '/../frontend/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+    res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
 });
 
 app.listen(config.PORT, () => {
-  console.log('Server started at http://localhost:5000');
+    console.log('Server started at http://localhost:5000');
 });
